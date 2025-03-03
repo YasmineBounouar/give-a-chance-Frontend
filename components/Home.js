6598455731b3a28deeaab4d5de1b89e9b4d54a37
sebai <sarah.sebai1612@gmail.com>
2025-03-03 17:48:10 +0100

    add structure frontend

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,66 @@
+import { Link } from 'next/Link';
+import {Image} from 'next/Image'
 import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+
+
+          <header className={styles.header}>
+          <Image></Image>
+          <Link href='#FAQ'><span>Home</span></Link>
+
+
+          <Link href='/home'><span></span></Link>
+          <Link href=''><span></span></Link>
+          <Link><span></span></Link>
+   
+          <button></button>
+          <span>ICON</span>
+          </header>
+
+          <section id='FAQ'>
+          <h1></h1>
+          <p></p>
+          <button>EXPLORE PROFILS</button>
+          <div>
+          <div>
+            <Link><span></span></Link>
+            <Link><span></span></Link>
+            <h5></h5>
+            <h6></h6>
+            <p></p>
+            <span>
+            <span>icon</span>
+            <p></p>
+            </span>
+          </div>
+          </div>
+          
+        </section>
+
+        </main>
+
+
+
+        <section>
+          <h2></h2>
+          
+        </section>
+
+
+
+
+
+
+        <section></section>
+        <section></section>
+
+        <footer></footer>
+        
+  
     </div>
   );
 }
